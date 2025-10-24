90a0ff182c1ace71998bf8b7c964fcc1f156472a
DJ WILMER <radiourbana00@gmail.com>
2025-10-23 20:55:22 -0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('radio-fama-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        'https://i.ibb.co/0Rm18DBQ/Se-al-informativa.jpg'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
